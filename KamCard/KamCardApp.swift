a1efed344aa61837607364f38ca0f10a2ba63ab7
Kamil Suwada <kamilsuwada@icloud.com>
2021-12-27 23:01:18 +0000

    Initial Commit

---
+++ KamCard/KamCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  KamCardApp.swift
+//  KamCard
+//
+//  Created by Kamil Suwada on 27/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct KamCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
