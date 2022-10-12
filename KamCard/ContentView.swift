db09939e0b7557f881116e16c693b5069125c4a6
Kamil Suwada <kamilsuwada@icloud.com>
2022-10-12 19:38:13 +0100

    feat: add final version of the app made during Udemy course by Dr Angela Yu.

--- KamCard/ContentView.swift
+++ KamCard/ContentView.swift
@@ -9,8 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            
+            Color(red: 0.09, green: 0.63, blue: 0.52)
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                Image("kam")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                
+                Text("Kamil Suwada")
+                    .font(.title)
+                    .font(.system(size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding(.all, 5.0)
+                    
+                Text("iOS Developer")
+                    .font(.headline)
+                    .font(.system(size: 25))
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding(.all, 5.0)
+                
+                Divider()
+                
+                InfoView(text: "+44 (0) 7519665641", imageName: "phone.fill")
+                InfoView(text: "kamilsuwada@icloud.com", imageName: "envelope.fill")
+            }
+        }
     }
 }
 
---
+++ KamCard/InfoView.swift
@@ -0,0 +1,34 @@
+//
+//  InfoView.swift
+//  KamCard
+//
+//  Created by Kamil Suwada on 27/12/2021.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 50)
+            .frame(height: 50)
+            .foregroundColor(.white)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.green)
+                Text(text)
+                    .font(.system(size: 20))
+            })
+            .padding(.all)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
